7177133b4ad6c70ae002e9a45dda90aefcfb1080
Rollo-the-coder <erikgotta@gmail.com>
2021-05-07 18:11:44 +0700

    Working webpackgit add -A! with css

---
+++ src/getClasses.js
@@ -0,0 +1,10 @@
+import { wizardz } from "./wizard";
+import { barbarian } from "./barbarian";
+
+function getClasses() {
+  console.log("get classes was called");
+  console.log(wizardz);
+  console.log(barbarian);
+}
+
+export default getClasses;
